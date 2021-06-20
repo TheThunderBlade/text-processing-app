2052e3f190262b2824fc0dbce7ef609a04825c0d
TheThunderBlade <denisrysak2001@gmail.com>
2021-06-20 07:23:01 +0300

    .

--- server/index.js
+++ server/index.js
@@ -1,14 +1,11 @@
 require('dotenv').config()
 const express = require('express')
 const router = require('./routes/routes')
-const path = require('path')
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
 app.use(express.json())
-
-app.use(express.static(path.join(__dirname, 'client/build')))
 
 app.use('/api', router)
 
